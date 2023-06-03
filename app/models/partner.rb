09068d2f215794ba7b9e5ede59672eb063b0d676
sdudnik <sdudnik@ukr.net>
2023-06-03 13:48:20 +0300

    rebuild menu and del two menu item and Partner class clear

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,7 +1,7 @@
 class Partner < ActiveRecord::Base
-	def self.find_partners_for_view
-		find(:all, :order => "id" )
-	end
-	has_many :measures
-	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	# def self.find_partners_for_view
+	# 	find(:all, :order => "id" )
+	# end
+	# has_many :measures
+	# has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
